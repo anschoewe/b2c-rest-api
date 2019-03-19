d5d2111eb3b384c3df485a291774df5020f0294c
Andrew Schoewe <andrew@schoewe.me>
2019-03-19 00:23:18 -0400

    created a default endpoint

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Hello
+# Build locally with gradle
+
+`./gradlew build && java -jar build/libs/b2c-rest-api-0.0.1-SNAPSHOT.jar`
+
+
--- src/main/java/me/schoewe/b2crestapi/SignInController.java
+++ src/main/java/me/schoewe/b2crestapi/SignInController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(path = "/signin")
+@RequestMapping(path = {"/signin",  "/"})
 public class SignInController
 {
 	
