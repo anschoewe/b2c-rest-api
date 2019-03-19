455b6aba8d07ff83f7ee43770bb70af153990d04
Andrew Schoewe <andrew@schoewe.me>
2019-03-18 23:00:56 -0400

    initial commit

---
+++ src/main/java/me/schoewe/b2crestapi/B2cRestApiApplication.java
@@ -0,0 +1,13 @@
+package me.schoewe.b2crestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class B2cRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(B2cRestApiApplication.class, args);
+	}
+
+}
---
+++ src/main/java/me/schoewe/b2crestapi/SignInController.java
@@ -0,0 +1,21 @@
+package me.schoewe.b2crestapi;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/signin")
+public class SignInController
+{
+	
+	@RequestMapping("/")
+	public String home() {
+		return "hello";
+	}
+	
+	@RequestMapping(method = RequestMethod.GET, path = "/doesUserExist")
+	public boolean doesUserExist() {
+		return true;
+	}
+}
