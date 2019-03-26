530f5aec2525a44f4ef15d2e1170cb551323bc7f
Andrew Schoewe <andrew@schoewe.me>
2019-03-26 14:08:04 -0400

    removed default path for controller

--- src/main/java/me/schoewe/b2crestapi/SignInController.java
+++ src/main/java/me/schoewe/b2crestapi/SignInController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(path = {"/signin",  "/"})
+@RequestMapping(path = {"/signin"})
 public class SignInController
 {
 	private final Logger logger = LoggerFactory.getLogger(this.getClass());
