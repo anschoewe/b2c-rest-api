b38126ed8b0042e546756bccba76118463a761ae
Andrew Schoewe <andrew@schoewe.me>
2019-03-26 14:47:41 -0400

    added default path

--- src/main/java/me/schoewe/b2crestapi/DefaultController.java
+++ src/main/java/me/schoewe/b2crestapi/DefaultController.java
@@ -6,12 +6,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(path = {"/"} )
+@RequestMapping(path = { "", "/" } )
 public class DefaultController {
 	private final Logger logger = LoggerFactory.getLogger(this.getClass());
 	
 	
-	@RequestMapping("/")
+	@RequestMapping(path = { "", "/" })
 	public String home() {
 		logger.info("in home");
 		logger.info(System.getProperty("buildNum"));
