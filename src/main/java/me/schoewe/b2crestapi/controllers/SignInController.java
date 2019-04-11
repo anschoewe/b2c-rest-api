2975e06fdc36a8e8820e6286b70b97998abde8a1
Andrew Schoewe <andrew@schoewe.me>
2019-04-11 16:49:10 -0400

    added comments around signin signup methods

--- src/main/java/me/schoewe/b2crestapi/controllers/SignInController.java
+++ src/main/java/me/schoewe/b2crestapi/controllers/SignInController.java
@@ -34,6 +34,15 @@ public class SignInController
 	@Autowired
 	private AzureB2CService b2cService;
 	
+	/**
+	 * If the user exists in the other IdP, then create them in B2C
+	 * Then return the the new B2C user Id.
+	 * If the user does NOT exist in the other/legacy IdP, it is assumed B2C will create them through the sign-up flow
+	 * 
+	 * @param input
+	 * @return
+	 * @throws JSONException
+	 */
 	@PostMapping(path= "/signin", consumes = "application/json", produces = "application/json")
 	public ResponseEntity<?> signin(@RequestBody SignInInputClaims input) throws JSONException {
 		//////////////////
@@ -57,22 +66,31 @@ public class SignInController
 		
 		//TODO: Query other IdP to get existing user details
 		
-		UserModel user = new UserModel();
-		user.setFirstName("Fake");
-		user.setLastName("User1");
-		user.setDisplayName("Fake User1");
-		user.setEmail(input.getEmail());
-		user.setOtherIdpUserId("externalIdpId_" + input.getEmail());
-		user.setPassword(input.getPassword());
+		// Example, pretend a user with 'OtherIdp.User1@example.com' email addresses already exists in legacy IdP
+		// For all other users, assume they don't exist and therefore we should NOT create the user in B2C
 		
-		// Create user in B2C since we did NOT find them in B2C but DID find them in the remote IdP
-		String userId = b2cService.createUser(user);
-
-        // Return the output claim(s)
 		SignInOutputClaims output = new SignInOutputClaims();
-		output.setEmail(input.getEmail());
-		output.setUserProfileId(userId);
 		
+		if(StringUtils.equalsAnyIgnoreCase(input.getEmail(), "OtherIdp.User1@example.com")) {
+			UserModel user = new UserModel();
+			user.setFirstName("OtherIdp");
+			user.setLastName("User1");
+			user.setDisplayName("OtherIdp User1");
+			user.setEmail(input.getEmail());
+			user.setOtherIdpUserId("externalIdpId_" + input.getEmail());
+			user.setPassword(input.getPassword());
+			
+			// Create user in B2C since we did NOT find them in B2C but DID find them in the remote IdP
+			String userId = b2cService.createUser(user);
+	        // Return the output claim(s)
+			output.setEmail(input.getEmail());
+			output.setUserProfileId(userId);
+		} else {
+			// user does not exist in legacy IdP, so don't create them in B2C automatically
+			output.setEmail(input.getEmail());
+			output.setUserProfileId("");
+		}
+
         return new ResponseEntity<>(output, HttpStatus.OK);
 	}
 	
@@ -102,8 +120,7 @@ public class SignInController
 //        outputClaims.setLoyaltyNumber("random123");
 
         // Return the output claim(s)
-        return new ResponseEntity<>(new B2CResponseContent("success", HttpStatus.OK),
-    			HttpStatus.OK);
+        return new ResponseEntity<>(new B2CResponseContent("success", HttpStatus.OK), HttpStatus.OK);
 	}
 	
 	@PostMapping(path= "/createRemoteUser", consumes = "application/json", produces = "application/json")
